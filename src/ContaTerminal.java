eeb80b85e6e86670cada0f253b7a872127b460ec
Thiago <thiagoesmael19@gmail.com>
2024-10-10 20:45:29 -0300

    alteração scanner close

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -23,6 +23,6 @@ public class ContaTerminal{
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque" );
 
-
+        scanner.close();
     }
 }
